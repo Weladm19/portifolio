0890b2aa2cde8712a94e4787bb7ceab8ed90b742
Weladm19 <wel.indumill@gmail.com>
2023-11-15 18:21:02 -0300

    MyFist Commit

---
+++ main.js
@@ -0,0 +1,19 @@
+window.mostrador = ScrollReveal({reset : true});
+
+mostrador.reveal('#txt', {
+    duration : 2000,
+    distance : "90px",
+})
+
+mostrador.reveal('#txt_img', {
+    duration : 2000,
+    distance : "90px",
+    delay: 700,
+})
+
+mostrador.reveal(".claDiv" ,{
+    duration : 2000,
+    distance : "90px",
+    delay: 300
+})
+
